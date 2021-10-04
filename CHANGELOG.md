434e122a0997b6cf4acb60f5e2c31a38c572c9ff
dylanagreen <dylan@mygreenfamily.us>
2021-10-04 12:21:59 -0700

    Add a version string and increment it to 3.0.0.dev.
    - Lots of changes I am about to make will break backwards compat and require a new major version.
    - Once these changes are complete QuasarNET will be at 3.0.0 and will NOT be backwards compat
    with old QuasarNET 2.0.0 code.

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+This the log of changes to [QuasarNET](https://github.com/dylanagreen/QuasarNET).
+
+All notable changes to this project will be documented in this file.
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [3.0.0] - Unreleased
+### Added
+
+### Changed
+
+## [2.0.0]
+Initial version received from James Farr.
--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
-
-import glob
-
+import os, sys, glob, re
 from setuptools import setup, find_packages
+
+def _get_version():
+    line = open('py/quasarnet/_version.py').readline().strip()
+    m = re.match("__version__\s*=\s*'(.*)'", line)
+    if m is None:
+        print('ERROR: Unable to parse version from: {}'.format(line))
+        version = 'unknown'
+    else:
+        version = m.groups()[0]
 
 scripts = glob.glob('bin/*')
 
 description = "CNN for quasar classification and redshifting"
 
-version="2.0"
 setup(name="quasarnet",
-      version=version,
+      version=_get_version(),
       description=description,
-      url="https://github.com/ngbusca/QuasarNET",
-      author="Nicolas Busca et al",
-      author_email="nbusca@gmail.com",
+      url="https://github.com/dylanagreen/QuasarNET",
+      author="Dylan Green, James Farr, Nicolas Busca et al",
+      author_email='dylanag@uci.edu',
       packages=['quasarnet'],
       package_dir = {'': 'py'},
       install_requires=['scipy','numpy',
