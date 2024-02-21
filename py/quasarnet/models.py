f5ba9efebb54d34801104f48f644075896ca6368
Dylan Green <dylanag@uci.edu>
2024-02-21 14:37:05 -0800

    Add 6 layer same padding model.

--- py/quasarnet/models.py
+++ py/quasarnet/models.py
@@ -3,23 +3,25 @@ import numpy as np
 
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense, Activation, BatchNormalization, Flatten, Conv1D, concatenate, Lambda, Dropout
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, load_model
 import tensorflow.keras.backend as K
 from tensorflow.keras.utils import plot_model
 from tensorflow.keras.initializers import glorot_uniform, glorot_uniform
 from tensorflow.keras import regularizers
 from tensorflow.keras.activations import softmax, relu
 
-print("version", tf.__version__)
+from pathlib import Path
 
 def QuasarNET(input_shape =  None, boxes = 13, nlines = 1, reg_conv = 0., reg_fc=0, offset_activation_function='rescaled_sigmoid'):
 
     X_input = Input(input_shape)
+    # X = X_input[:, :-5, :]
+    # X_phot = X_input[:, -5:, :]
     X = X_input
 
     # Set the parameters.
     ## Number of convolutional layers
-    nlayers = 4
+    nlayers = 6
     ## Number of filters in convolutional layers.
     nfilters_conv = 100
     ## Max number of filters per layer.
@@ -32,25 +34,23 @@ def QuasarNET(input_shape =  None, boxes = 13, nlines = 1, reg_conv = 0., reg_fc
     d_rate = 0.2
 
     filter_arr = [2, 4, 8, 12]
-#     filter_arr = [20, 15, 10, 6]
-#     filter_arr = [5, 19, 19, 5]
 
     # Set up the convolutional layers.
     for stage in range(nlayers):
         ## Convolutional layer with glorot_uniform initial weights, regularised
         ## with an l2 norm (set to 0 by default).
-#         nfilters = filter_arr[stage]
+        # nfilters = filter_arr[stage]
         nfilters = nfilters_conv
-
-        X = Conv1D(nfilters, filter_size, strides = strides,
+        stage_stride = strides
+        X = Conv1D(nfilters, filter_size, strides = stage_stride,
                 name = 'conv_{}'.format(stage+1),
                 kernel_initializer=glorot_uniform(),
-                kernel_regularizer=regularizers.l2(reg_conv))(X)
+                kernel_regularizer=regularizers.l2(reg_conv),
+                  padding="same")(X)
         ## Batch normalise in features (axis=-1).
         X = BatchNormalization(axis=-1)(X)
         ## Apply relu activation.
         X = Activation('relu')(X)
-
         X = Dropout(rate=d_rate)(X)
 
     # Set up the final, fully-connected layer, batch normalising and applying a
@@ -59,8 +59,6 @@ def QuasarNET(input_shape =  None, boxes = 13, nlines = 1, reg_conv = 0., reg_fc
     X = Dense(nfilters_max, activation='linear', name='fc_common')(X)
     X = BatchNormalization()(X)
     X = Activation('relu', name='fc_activation')(X)
-
-#     X = Dropout(rate=d_rate)(X)
 
     # Build the "feature detection" units for each line.
     outputs = []
@@ -73,7 +71,7 @@ def QuasarNET(input_shape =  None, boxes = 13, nlines = 1, reg_conv = 0., reg_fc
         tf_activation_function = 'linear'
     for i in range(nlines):
         ## Set up the boxes to determine the coarse location of the line.
-        X_box_aux = Dense(boxes, activation='sigmoid',
+        X_box_aux = Dense(boxes, activation=tf_activation_function,
                 name='fc_box_{}'.format(i),
                 kernel_initializer=glorot_uniform())(X)
 
